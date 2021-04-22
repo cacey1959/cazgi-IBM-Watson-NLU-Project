4292079ab309d37b1810d6667f3ed5abfb24d2c3
cacey1959 <81583051+cacey1959@users.noreply.github.com>
2021-04-22 08:12:57 -0400

    Update EmotionTable.js

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-      render() {
-    const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+    render() {
+      const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
     let li_ctr = 0;
     return (
       <div style={colorStyle}>
         React Component
             <ul>
-                {this.state.eventList.map(function(eventDetails){
-                    return <li key={li_ctr++}>{eventDetails}</li>;
+               
+                {this.state.sentimentOutput.map(function(getEmotion){
+                    return <li key={li_ctr++}>{getEmotion}</li>;
                   })}
             </ul>      
         </div>
     );
-  }
-}
-export default EmotionTable;
+
+        }
